a5b67a7a8217602ea879047a32e4e6c52347a4ee
SamarjitBhogal <samarjit.v.bhogal@gmail.com>
2026-02-13 14:15:55 -0800

    Completed basic selenium setup.

---
+++ HRMgmtTest/pages/BasePage.cs
@@ -0,0 +1,19 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+public class BasePage
+{
+    protected readonly IWebDriver _driver;
+    protected readonly WebDriverWait _wait;
+
+    public BasePage(IWebDriver driver)
+    {
+        _driver = driver;
+        _wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+    }
+    
+    public void CloseBrowser()
+    {
+        _driver.Quit();
+    }
+}
---
+++ HRMgmtTest/tests/unit/ExampleTest.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium.Chrome;
+
+namespace HRMgmtTest;
+
+public class Tests
+{
+    private BasePage basePage;
+
+    [SetUp]
+    public void Setup()
+    {
+        // Initialize WebDriver and page object file here (doesn't have to be BasePage)
+        // For example:
+        basePage = new BasePage(new ChromeDriver());
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        // Clean up WebDriver here
+        basePage.CloseBrowser();
+    }
+}
