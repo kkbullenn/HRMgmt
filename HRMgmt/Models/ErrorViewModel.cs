cb46f551eb6cde9e0a3351b630388cb6f1235cb0
LEGACY21\tkngu <tknguyen2111@gmail.com>
2026-02-13 14:07:54 -0800

    Modify Models to match new DB diagram design

--- HRMgmt/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HRMgmt.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- HRMgmt/OrgDbContext.cs
+++ HRMgmt/OrgDbContext.cs
@@ -7,27 +7,46 @@ namespace HRMgmt
         public OrgDbContext(DbContextOptions<OrgDbContext> options) : base(options) { }
         public OrgDbContext() { }
 
-        public DbSet<Employee> Employees { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
         public DbSet<Shift> Shifts { get; set; }
         public DbSet<EmployeeShift> EmployeeShifts { get; set; }
-        public DbSet<ShiftTemplate> ShiftTemplates { get; set; }
+        public DbSet<ShiftAssignment> ShiftAssignments { get; set; }
+        public DbSet<Payroll> Payrolls { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<EmployeeShift>()
-                .HasKey(es => new { es.EmployeeId, es.ShiftId, es.Date });
+            modelBuilder.Entity<User>()
+                .HasOne<Role>()    
+                .WithMany()               
+                .HasForeignKey(u => u.Role);
 
             modelBuilder.Entity<EmployeeShift>()
-                .HasOne(es => es.Employee)
-                .WithMany(e => e.EmployeeShifts)
-                .HasForeignKey(es => es.EmployeeId);
+                .HasOne<User>()
+                .WithMany()
+                .HasForeignKey(es => es.UserId);
 
             modelBuilder.Entity<EmployeeShift>()
-                .HasOne(es => es.Shift)
+                .HasOne<Shift>()
                 .WithMany(s => s.EmployeeShifts)
                 .HasForeignKey(es => es.ShiftId);
+
+            modelBuilder.Entity<ShiftAssignment>()
+                .HasOne<User>()
+                .WithMany()
+                .HasForeignKey(sa => sa.UserId);
+
+            modelBuilder.Entity<ShiftAssignment>()
+                .HasOne<Shift>()
+                .WithMany()
+                .HasForeignKey(sa => sa.ShiftId);
+
+            modelBuilder.Entity<Payroll>()
+                .HasOne<User>()
+                .WithMany()
+                .HasForeignKey(p => p.UserId);
         }
     }
 
